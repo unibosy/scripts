4ef045914708fee6c45f27b68307dc41b5063eef
unibosy <unibosy@gmail.com>
2018-06-13 16:59:41 +0800

    add example of source xxx.sh to make env sense

---
+++ linux/commom/checkout_count.sh
@@ -0,0 +1,1 @@
+(find . -name "*.aac" | wc -l)&&(find . -name "*.mp4" | wc -l)
---
+++ linux/commom/setVar.sh
@@ -0,0 +1,3 @@
+#source xxx.sh to make environment sense
+MYTEST=/home/unibosy/ws/mygit/scripts/linux/commom
+export MYTEST
